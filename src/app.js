d882b7ed47f5e658b460a849bbb749b1513b8700
sonalibhalla98 <sonalibhalla98@gmail.com>
2023-03-30 18:21:03 +0530

    c

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./app.css";
 
 const App = () =>{
     return (
-        <h1 className="header" style={{color:"blue"}}>
+        <h1 style={{color:"blue"}}>
             Hello world! I am using Remote Directory
         </h1>
     )
